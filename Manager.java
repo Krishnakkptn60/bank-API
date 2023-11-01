dc855fdd60deaa427345a57175192ecdbefea098
Krishnakkptn60 <129956292+Krishnakkptn60@users.noreply.github.com>
2023-11-01 14:48:48 +0530

    Bank API
    all files

---
+++ Manager.java
@@ -0,0 +1,48 @@
+package com.bank.app.bankappusingspringboot.dto;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Component
+@Getter
+@Setter
+public class Manager 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int managerId;
+	@NotBlank(message="managername cannot be blank")
+	@NotNull(message="managername cannot be  null")
+	private String managerName;
+	@Positive
+	@Min(value=6000000000l)
+	@Max(value=9999999999l)
+	private long managerContact;
+	@Email(message = "Email is not valid", regexp="{(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])}")
+	@NotNull(message = "Email cannot be empty")
+	private String managerEmailid;
+	@Pattern(regexp="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$", message="must contain one Capital letter,one small letter,one number,one special letter" )
+	private String managerPassword;
+	@OneToOne()
+	@JsonIgnore
+	private Bank bank;
+	
+
+}
